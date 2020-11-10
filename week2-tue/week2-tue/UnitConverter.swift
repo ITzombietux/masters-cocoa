3db3318fb52f3854ffa4885d3da77de5745b087d
zombietux <>
2020-11-10 16:57:13 +0900

    단위 변환 model <-> view

--- week2-tue/week2-tue/UnitConverter.swift
+++ week2-tue/week2-tue/UnitConverter.swift
@@ -10,9 +10,9 @@ import Foundation
 struct UnitConverter {
     private (set) var result: String = ""
  
-    mutating func convert(value: Double, fromUnit: Unit, toUnit: Unit) {
-        let measurement = Measurement(value: value , unit: UnitLength.centimeters)
-        let convertedValue = measurement.converted(to: .yards)
+    mutating func convert(value: Double, fromUnit: Dimension, toUnit: Dimension) {
+        let measurement = Measurement(value: value , unit: fromUnit)
+        let convertedValue = measurement.converted(to: toUnit)
         
         result = "\(convertedValue)"
     }
--- week2-tue/week2-tue/ViewController.swift
+++ week2-tue/week2-tue/ViewController.swift
@@ -5,15 +5,6 @@
 //  Created by zombietux on 2020/11/10.
 //
 
-/*
- let street1 = Measurement(value: 73, unit: UnitLength.meters)
- let street2 = Measurement(value: 67, unit: UnitLength.meters)
- var commuteDistance = street1 + street2
- commuteDistance.convert(to: UnitLength.yards)
- 
- 단위 목록 문서를 보면 여러 길이, 부피, 무게 단위가 만들어져 있다. 이 중에 하나를 정해서 단위를 변환하는 앱을 구현한다.
- */
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -31,7 +22,7 @@ class ViewController: UIViewController {
         
         var unitConverter = UnitConverter()
         unitConverter.convert(value: value ?? 0, fromUnit: UnitLength.centimeters, toUnit: UnitLength.yards)
-        resultLabel.text = "\(unitConverter.result)"
+        resultLabel.text = "결과: \(unitConverter.result)"
     }
 }
 
